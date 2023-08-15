970367e7829ea0cdcdf8f0d0d5db128a23f088b0
unknown <aeartixenter@gmail.com>
2023-08-15 18:32:25 +0800

    configured multiple entry for each calculators

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('index.html', import.meta.url)),
+        capitalGains: fileURLToPath(new URL('./src/capital-gains/index.html', import.meta.url)),
+        mortgageCrusher: fileURLToPath(new URL('./src/mortgage-crusher/index.html', import.meta.url)),
+      }
+    }
+  }
 });
